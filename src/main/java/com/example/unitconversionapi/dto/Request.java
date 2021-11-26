b2cdd0bf37909395e11792d86514916c677735c8
jakobCan <jakob.can.code@gmail.com>
2021-11-26 11:58:52 +0100

    cleans up project after new input from Marvin (DTOs)

--- src/main/java/com/example/unitconversionapi/dto/Request.java
+++ src/main/java/com/example/unitconversionapi/dto/Request.java
@@ -2,5 +2,37 @@ package com.example.unitconversionapi.dto;
 
 public class Request {
 
+    private float fromValue;
+    private String fromType;
+    private String toType;
 
+    public Request(float fromValue, String fromType, String toType) {
+        this.fromValue = fromValue;
+        this.fromType = fromType;
+        this.toType = toType;
+    }
+
+    public float getFromValue() {
+        return fromValue;
+    }
+
+    public void setFromValue(float fromValue) {
+        this.fromValue = fromValue;
+    }
+
+    public String getFromType() {
+        return fromType;
+    }
+
+    public void setFromType(String fromType) {
+        this.fromType = fromType;
+    }
+
+    public String getToType() {
+        return toType;
+    }
+
+    public void setToType(String toType) {
+            this.toType = toType;
+        }
 }
