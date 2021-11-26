eea67908dbe42a3a51de31ccc75c9156d0c61d17
jakobCan <jakob.can.code@gmail.com>
2021-11-26 11:58:52 +0100

    cleans up project after new input from Marvin (DTOs)

--- src/main/java/com/example/unitconversionapi/controller/ConversionController.java
+++ src/main/java/com/example/unitconversionapi/controller/ConversionController.java
@@ -1,27 +1,21 @@
 package com.example.unitconversionapi.controller;
 
-import com.example.unitconversionapi.model.ConversionData;
+import com.example.unitconversionapi.dto.Request;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.validation.Valid;
 
 @RestController
 public class ConversionController {
 
-    public static void main(String[] args) throws ScriptException {
-        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-        ScriptEngine engine = scriptEngineManager.getEngineByName("JavaScript");
-        System.out.println(engine.eval("x+4".replace("x", "9")));
-    }
-
     @PostMapping("/convert")
-    public Float convert(@RequestBody @Valid ConversionData conversionData) throws ScriptException {
-
+    public Float convert(@RequestBody @Valid Request request) {
+        // TODO: 26.11.2021 Wir lesen aus dem Request-Objekt aus, welcher Typ in welchen konvertiert werden soll.
+        //  Welcher Service / Controller übernimmt die Logik, dass der richtige conversion-Service aufgerufen wird?
+        //  Und macht man das mit switch-case oder if-else? Ist das schön?
 
         return null;
     }
--- src/main/java/com/example/unitconversionapi/dto/Response.java
+++ src/main/java/com/example/unitconversionapi/dto/Response.java
@@ -1,4 +1,18 @@
 package com.example.unitconversionapi.dto;
 
 public class Response {
+
+    private float result;
+
+    public Response(float result) {
+        this.result = result;
+    }
+
+    public float getResult() {
+        return result;
+    }
+
+    public void setResult(float result) {
+        this.result = result;
+    }
 }
