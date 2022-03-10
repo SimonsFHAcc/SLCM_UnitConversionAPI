71547813f8d3d5f37b2e4fe44a8bd64ea20daac4
jakobCan <jakob.can.code@gmail.com>
2022-03-10 14:57:09 +0100

    response erweitert

--- src/main/java/com/example/unitconversionapi/controller/ConversionController.java
+++ src/main/java/com/example/unitconversionapi/controller/ConversionController.java
@@ -3,12 +3,9 @@ package com.example.unitconversionapi.controller;
 import com.example.unitconversionapi.conversion.ConversionService;
 import com.example.unitconversionapi.dto.Request;
 import com.example.unitconversionapi.dto.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.script.ScriptException;
 import javax.validation.Valid;
 
 @RestController
@@ -22,6 +19,6 @@ public class ConversionController {
 
     @PostMapping("/convert")
     public Response convert(@RequestBody @Valid Request request) {
-        return new Response(conversionService.convert(request));
+        return new Response(request, conversionService.convert(request));
     }
 }
--- src/main/java/com/example/unitconversionapi/dto/Response.java
+++ src/main/java/com/example/unitconversionapi/dto/Response.java
@@ -2,10 +2,22 @@ package com.example.unitconversionapi.dto;
 
 public class Response {
 
+    private boolean valid;
     private float result;
+    private float fromValue;
+    private String fromType;
+    private String toType;
 
     public Response(float result) {
         this.result = result;
+    }
+
+    public Response(Request request, float result){
+        this.valid = true;
+        this.result = result;
+        this.fromValue = request.getFromValue();
+        this.fromType = request.getFromType();
+        this.toType = request.getToType();
     }
 
     public float getResult() {
