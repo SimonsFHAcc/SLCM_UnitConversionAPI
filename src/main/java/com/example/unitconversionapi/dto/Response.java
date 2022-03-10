84008377e6c8f47ab6606c941c1c92708888a2f7
Stephan Podest <stephan.podest@gmail.com>
2022-03-10 14:24:15 +0100

    added values to response class

--- src/main/java/com/example/unitconversionapi/dto/Response.java
+++ src/main/java/com/example/unitconversionapi/dto/Response.java
@@ -2,10 +2,19 @@ package com.example.unitconversionapi.dto;
 
 public class Response {
 
+    private boolean valid;
     private float result;
+    private float fromValue;
+    private String fromType;
+    private String toType;
 
-    public Response(float result) {
+
+    public Response(boolean valid, float result, float fromValue, String fromType, String toType) {
+        this.valid = valid;
         this.result = result;
+        this.fromValue = fromValue;
+        this.fromType = fromType;
+        this.toType = toType;
     }
 
     public float getResult() {
